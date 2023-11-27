2eb7112381e319ab458257fb60b545e2129b33cb
Leon Erasmus <leondevcode@gmail.com>
2023-11-27 12:58:10 +0200

    first commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
  
-export const inter = Inter({ subsets: ['latin'] });
- 
-export const lusitana = Lusitana({
+// Ensure that you are importing from the correct package and use default imports
+export const inter = new Inter({ subsets: ['latin'] });
+export const lusitana = new Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });

de830e5583c9d90bcfc63a34c0883184e341215f
Leon Erasmus <leondevcode@gmail.com>
2023-11-29 15:14:37 +0200

    server side validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -121,7 +121,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
             {state.errors?.customerId &&
               state.errors.customerId.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
